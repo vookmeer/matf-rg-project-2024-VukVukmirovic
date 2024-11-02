154977ae241d1ce6b340051565d25cc10cca4776
mspasic <marko.spasic@oracle.com>
2024-11-02 22:27:53 +0100

    Add defer and overloaded helper functions.

--- rg-engine/include/engine/util/Utils.hpp
+++ rg-engine/include/engine/util/Utils.hpp
@@ -12,6 +12,35 @@
 
 #include <json.hpp>
 
+template<class... Ts>
+struct overloaded : Ts... {
+    using Ts::operator()...;
+};
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
+template<typename Func>
+struct DeferImpl {
+    DeferImpl(Func f) : f(f) {
+    }
+    ~DeferImpl() {
+        f();
+    }
+    Func f;
+};
+
+struct MakeDeferImpl {};
+
+template<typename Func>
+DeferImpl<Func> operator<<(MakeDeferImpl, Func f) {
+    return DeferImpl(f);
+}
+
+#define CONCAT_IMPL(a, b) a##b
+#define CONCAT(a, b) CONCAT_IMPL(a, b)
+#define defer auto CONCAT(defer_stmt_, __LINE__) = MakeDeferImpl() << [&]
+
+
 namespace rg {
 
     class Configuration {
