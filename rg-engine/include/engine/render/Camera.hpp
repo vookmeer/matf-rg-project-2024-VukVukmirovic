ebcb0812e1a13efcf26d2b4ee411d82ab895884a
spaske00 <matf.marko.spasic@gmail.com>
2024-11-30 21:55:01 +0100

    Add basic camera implementation.

---
+++ rg-engine/include/engine/render/Camera.hpp
@@ -0,0 +1,73 @@
+//
+// Created by spaske on 11/30/24.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+namespace rg {
+// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+// Default camera values
+constexpr float YAW         = -90.0f;
+constexpr float PITCH       =  0.0f;
+constexpr float SPEED       =  2.5f;
+constexpr float SENSITIVITY =  0.1f;
+constexpr float ZOOM        =  45.0f;
+
+
+// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+class Camera
+{
+public:
+    // camera Attributes
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+    // euler Angles
+    float Yaw;
+    float Pitch;
+    // camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
+
+    // constructor with vectors
+    explicit Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
+    // constructor with scalar values
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+
+    // returns the view matrix calculated using Euler Angles and the LookAt Matrix
+    glm::mat4 get_view_matrix() const;
+
+    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    void process_keyboard(Camera_Movement direction, float deltaTime);
+
+    // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    void process_mouse_movement(float xoffset, float yoffset, bool constrainPitch = true);
+
+    // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    void process_mouse_scroll(float yoffset);
+
+private:
+    // calculates the front vector from the Camera's (updated) Euler Angles
+    void update_camera_vectors();
+};
+
+}
+#endif //CAMERA_H
