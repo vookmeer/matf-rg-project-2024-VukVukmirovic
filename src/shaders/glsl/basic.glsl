bf42bbc50c0723b0c3ef6e3f583644e74b061c5e
spaske00 <matf.marko.spasic@gmail.com>
2024-12-01 12:55:03 +0100

    Add basic.glsl shader.

---
+++ src/shaders/glsl/basic.glsl
@@ -0,0 +1,35 @@
+#shader vertex
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+out vec2 TexCoords;
+out vec3 Normal;
+out vec3 FragPos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = aNormal;
+    TexCoords = aTexCoords;
+    gl_Position = projection * view * vec4(FragPos, 1.0);
+}
+
+#shader fragment
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D texture_diffuse1;
+
+void main() {
+    FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb, 1.0);
+}
