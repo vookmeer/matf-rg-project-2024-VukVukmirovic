5181af44269efd833fd1ba6cea922f4604263de3
spaske00 <matf.marko.spasic@gmail.com>
2024-12-02 17:13:57 +0100

    Create AppStateController that holds all the state

---
+++ include/AppStateController.hpp
@@ -0,0 +1,46 @@
+//
+// Created by spaske on 12/2/24.
+//
+
+#ifndef APPSTATECONTROLLER_HPP
+#define APPSTATECONTROLLER_HPP
+#include <engine/controller/Controller.hpp>
+#include <engine/platform/Platform.hpp>
+#include <engine/render/Camera.hpp>
+
+class AppStateController : public rg::Controller {
+public:
+
+    static std::unique_ptr<AppStateController> create();
+
+    std::string_view name() const override { return "AppStateController";}
+
+    rg::Camera *camera() {
+        return &m_camera;
+    }
+
+    const glm::mat4& projection() const {
+        return m_projection;
+    }
+
+private:
+    void initialize() override;
+
+    void terminate() override;
+
+    bool loop() override;
+
+    void update() override;
+
+    void poll_events() override;
+
+    void draw() override;
+
+    void update_camera();
+
+    glm::mat4 m_projection{};
+    rg::Camera m_camera;
+    rg::PlatformController * m_platform_controller{};
+};
+
+#endif //APPSTATECONTROLLER_HPP
