4c54891ac1fba001d427466b876d56178dfd4958
spaske00 <matf.marko.spasic@gmail.com>
2024-12-10 15:59:19 +0100

    PlatformController docs and reorganize.

--- engine/include/engine/platform/PlatformEventObserver.hpp
+++ engine/include/engine/platform/PlatformEventObserver.hpp
@@ -4,12 +4,27 @@
 #include <engine/platform/Input.hpp>
 
 namespace engine::platform {
+    /**
+    * @class PlatformEventObserver
+    * @brief Platform events callback object.
+    * Extend this class and override the methods you want to be called by the @ref PlatformController, then
+    * register an instance with @ref PlatformController::register_platform_event_observer.
+    */
     class PlatformEventObserver {
     public:
+        /**
+        * @brief Called by @ref PlatformController for every frame in which the mouse moved.
+        */
         virtual void on_mouse_move(MousePosition position);
 
+        /**
+        * @brief Called by @ref PlatformController for every frame in an event occured on the keyboard or mouse key.
+        */
         virtual void on_key(Key key);
 
+        /**
+        * @brief Called by @ref PlatformController when the window is resized.
+        */
         virtual void on_window_resize(int width, int height);
 
         virtual ~PlatformEventObserver() = default;
