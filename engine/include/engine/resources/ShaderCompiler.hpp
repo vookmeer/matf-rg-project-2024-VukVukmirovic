0815bdba4884f3d2157cc83f12dfce79ed5e4233
spaske00 <matf.marko.spasic@gmail.com>
2024-12-10 18:06:47 +0100

    ShaderCompiler doc.

--- engine/include/engine/resources/ShaderCompiler.hpp
+++ engine/include/engine/resources/ShaderCompiler.hpp
@@ -7,34 +7,89 @@
 #include <string>
 
 namespace engine::resources {
-    struct ShaderParsingResult {
-        std::string vertex_shader;
-        std::string fragment_shader;
-        std::string geometry_shader;
-    };
+	struct ShaderParsingResult {
+		std::string vertex_shader;
+		std::string fragment_shader;
+		std::string geometry_shader;
+	};
 
-    class ShaderCompiler {
-    public:
-        static Shader compile_from_source(std::string shader_name, std::string shader_source);
+	/**
+	* @class ShaderCompiler
+	* @brief Compiles GLSL shaders from a single source file.
+	* Vertex, Fragment and Geometry shaders are seperated by the `// #shader vertex|fragment|geometry` directive.
+	* All the code following the directive belongs to the source of the shader specified in the #shader directive.
+	* Here is an example:
+	* @code
+	* //#shader vertex
+	* #version 330 core
+	* layout (location = 0) in vec3 aPos;
+	*
+	* void main() {
+	*     gl_Position = vec4(aPos, 1.0);
+	* }
+	*
+	* //#shader fragment
+	* #version 330 core
+	* out vec4 FragColor;
+	*
+	* void main() {
+	*     FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+	* }
+	* @endcode
+	*/
+	class ShaderCompiler {
+	public:
+		/**
+		* @brief Compiles a shader from source.
+		* @param shader_name
+		* @param shader_source string for the vertex, fragment, [geometry] shader
+		* @returns Compiled @ref Shader object that can be used for drawing.
+		*/
+		static Shader compile_from_source(std::string shader_name, std::string shader_source);
 
-        static Shader compile_from_file(std::string shader_name, const std::filesystem::path &shader_path);
+		/**
+		* @brief Compiles a shader from file.
+		* @param shader_name
+		* @param shader_path containing the source for the vertex, fragment, [geometry] shader
+		* @returns Compiled @ref Shader object that can be used for drawing.
+		*/
+		static Shader compile_from_file(std::string shader_name, const std::filesystem::path &shader_path);
 
-        ShaderParsingResult parse_source();
+		/**
+		* @brief Splits a single shader source string into `vertex`, `fragment`, [`geometry`] shader strings.
+		* @returns @ref ShaderParsingResult
+		*/
+		ShaderParsingResult parse_source();
 
-        explicit ShaderCompiler(std::string shader_name, std::string shader_source) : m_shader_name(
-                std::move(shader_name))
-          , m_sources(std::move(shader_source)) {
-        }
+	private:
+		/**
+		* @brief Compile shader sources into a OpenGL shader program.
+		* @returns A @ref graphics::OpenGL::ShaderProgramId referencing OpenGL shader program.
+		*/
+		graphics::OpenGL::ShaderProgramId compile(const ShaderParsingResult &shader_sources);
 
-        graphics::OpenGL::ShaderProgramId compile(const ShaderParsingResult &shader_sources);
+		ShaderCompiler(std::string shader_name, std::string shader_source) : m_shader_name(
+			                                                                   std::move(shader_name))
+		                                                                 , m_sources(std::move(shader_source)) {
+		}
 
-    private:
-        std::string *now_parsing(ShaderParsingResult &result, const std::string &line);
+		/**
+		* @brief Returns the output field from the @ref ShaderParsingResult for which to continue appending `line`s of the `m_sources`.
+		* Detects if the line contains // #shader directive and returns a pointer to the appropriate
+		* field in the @ref ShaderParsingResult.
+		*/
+		std::string *now_parsing(ShaderParsingResult &result, const std::string &line);
 
-        uint32_t compile(const std::string &shader_source, ShaderType type);
+		/**
+		* @brief Compiles a single shader (Vertex|Fragment|Geometry) (not the whole program).
+		* @param shader_source
+		* @param type of the shader to compile Vertex|Fragment|Geometry
+		* @returns OpenGL id to a compiled shader.
+		*/
+		uint32_t compile(const std::string &shader_source, ShaderType type);
 
-        std::string m_shader_name;
-        std::string m_sources;
-    };
+		std::string m_shader_name;
+		std::string m_sources;
+	};
 }
 #endif //SHADER_COMPILER_HPP
